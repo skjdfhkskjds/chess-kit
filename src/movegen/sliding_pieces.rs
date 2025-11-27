cd6bef1b347d121f640c1233be6cf3037a0a7095
skjdfhkskjds <colin.chuu@gmail.com>
2025-11-27 14:18:37 -0500

    refactor the sliding pieces functions

--- src/movegen/sliding_pieces.rs
+++ src/movegen/sliding_pieces.rs
@@ -1,11 +1,10 @@
-use crate::movegen::{
-    BISHOP_MAGIC_NUMS, BISHOP_TABLE_SIZE, Magic, MoveGenerator, ROOK_MAGIC_NUMS, ROOK_TABLE_SIZE,
-};
+use crate::movegen::MoveGenerator;
 use crate::primitives::{
-    BITBOARD_FILES, BITBOARD_RANKS, BITBOARD_SQUARES, Bitboard, Files, Piece, Pieces, Ranks,
-    Square, Squares, BitboardVec,
+    BITBOARD_FILES, BITBOARD_RANKS, BITBOARD_SQUARES, Bitboard, BitboardVec, Files, Ranks, Square,
 };
 
+// Direction is an enum that represents the movement direction of a sliding
+// piece.
 pub enum Direction {
     Up,
     Right,
@@ -23,11 +22,11 @@ impl MoveGenerator {
     // @param: square - square to get the mask for
     // @return: masking bitboard for the given square
     pub fn rook_mask(square: Square) -> Bitboard {
-        let bb_rook_square = BITBOARD_SQUARES[square.unwrap()];
-        let bb_edges = MoveGenerator::get_edges(square);
-        let bb_mask = BITBOARD_FILES[square.file()] | BITBOARD_RANKS[square.rank()];
+        let rook_at = BITBOARD_SQUARES[square.unwrap()];
+        let edges = MoveGenerator::get_edges(square);
+        let mask = BITBOARD_FILES[square.file()] | BITBOARD_RANKS[square.rank()];
 
-        bb_mask & !bb_edges & !bb_rook_square
+        mask & !edges & !rook_at
     }
 
     // bishop_mask returns the bishop mask for the given square
@@ -37,57 +36,54 @@ impl MoveGenerator {
     #[inline(always)]
     pub fn bishop_mask(square: Square) -> Bitboard {
         let bitboard = Bitboard::empty();
-        let bb_bishop_square = BITBOARD_SQUARES[square.unwrap()];
-        let bb_edges = MoveGenerator::get_edges(square);
-        let bb_up_left = MoveGenerator::bb_ray(&bitboard, square, Direction::UpLeft);
-        let bb_up_right = MoveGenerator::bb_ray(&bitboard, square, Direction::UpRight);
-        let bb_down_right = MoveGenerator::bb_ray(&bitboard, square, Direction::DownRight);
-        let bb_down_left = MoveGenerator::bb_ray(&bitboard, square, Direction::DownLeft);
+        let bishop_at = BITBOARD_SQUARES[square.unwrap()];
+        let edges = MoveGenerator::get_edges(square);
+        let attacking = MoveGenerator::attack_ray(&bitboard, square, Direction::UpLeft)
+            | MoveGenerator::attack_ray(&bitboard, square, Direction::UpRight)
+            | MoveGenerator::attack_ray(&bitboard, square, Direction::DownRight)
+            | MoveGenerator::attack_ray(&bitboard, square, Direction::DownLeft);
 
-        (bb_up_left | bb_up_right | bb_down_right | bb_down_left) & !bb_edges & !bb_bishop_square
+        attacking & !edges & !bishop_at
     }
 
-    // TODO: think about moving this function elsewhere
-    fn get_edges(exclude: Square) -> Bitboard {
-        let exclude_file = BITBOARD_FILES[exclude.file()];
-        let exclude_rank = BITBOARD_RANKS[exclude.rank()];
-
-        (BITBOARD_FILES[Files::A] & !exclude_file)
-            | (BITBOARD_FILES[Files::H] & !exclude_file)
-            | (BITBOARD_RANKS[Ranks::R1] & !exclude_rank)
-            | (BITBOARD_RANKS[Ranks::R8] & !exclude_rank)
-    }
-
-    // This function takes a square, and all the blocker boards belonging
-    // to that square. Then it'll iterate through those blocker boards, and
-    // generate the attack board belonging to that blocker board.
+    // rook_attack_boards returns the attack boards for the given square and
+    // blockers.
+    // 
+    // @param: square - square to get the attack boards for
+    // @param: blockers - blockers to use to generate the attack boards
+    // @return: attack boards for the given square and blockers
     pub fn rook_attack_boards(square: Square, blockers: &[Bitboard]) -> BitboardVec {
         let mut attacks: BitboardVec = Vec::new();
 
         for bitboard in blockers.iter() {
-            let attacking = MoveGenerator::bb_ray(bitboard, square, Direction::Up)
-                | MoveGenerator::bb_ray(bitboard, square, Direction::Right)
-                | MoveGenerator::bb_ray(bitboard, square, Direction::Down)
-                | MoveGenerator::bb_ray(bitboard, square, Direction::Left);
+            let attacking = MoveGenerator::attack_ray(bitboard, square, Direction::Up)
+                | MoveGenerator::attack_ray(bitboard, square, Direction::Right)
+                | MoveGenerator::attack_ray(bitboard, square, Direction::Down)
+                | MoveGenerator::attack_ray(bitboard, square, Direction::Left);
             attacks.push(attacking);
         }
 
         attacks
     }
 
-    // Same as the function above, but for the bishop.
+    // bishop_attack_boards returns the attack boards for the given square and
+    // blockers.
+    // 
+    // @param: square - square to get the attack boards for
+    // @param: blockers - blockers to use to generate the attack boards
+    // @return: attack boards for the given square and blockers
     pub fn bishop_attack_boards(square: Square, blockers: &[Bitboard]) -> BitboardVec {
-        let mut bb_attack_boards: BitboardVec = Vec::new();
+        let mut attacks: BitboardVec = Vec::new();
 
-        for b in blockers.iter() {
-            let bb_attacks = MoveGenerator::bb_ray(b, square, Direction::UpLeft)
-                | MoveGenerator::bb_ray(b, square, Direction::UpRight)
-                | MoveGenerator::bb_ray(b, square, Direction::DownRight)
-                | MoveGenerator::bb_ray(b, square, Direction::DownLeft);
-            bb_attack_boards.push(bb_attacks);
+        for bitboard in blockers.iter() {
+            let attacking = MoveGenerator::attack_ray(bitboard, square, Direction::UpLeft)
+                | MoveGenerator::attack_ray(bitboard, square, Direction::UpRight)
+                | MoveGenerator::attack_ray(bitboard, square, Direction::DownRight)
+                | MoveGenerator::attack_ray(bitboard, square, Direction::DownLeft);
+            attacks.push(attacking);
         }
 
-        bb_attack_boards
+        attacks
     }
 
     // blocker_boards() takes a piece mask. This is a bitboard in which all
@@ -96,6 +92,7 @@ impl MoveGenerator {
     // blocker_boards() generates all possible permutations for the given
     // mask, using the Carry Rippler method. See the given link, or
     // http://rustic-chess.org for more information.
+    // TODO: revisit this function later
     pub fn blocker_boards(mask: Bitboard) -> BitboardVec {
         let mut bb_blocker_boards: BitboardVec = Vec::new();
         let mut n: Bitboard = Bitboard::empty();
@@ -113,93 +110,124 @@ impl MoveGenerator {
         bb_blocker_boards
     }
 
-    // This is a long function, but fortunately it's easy to understand. It
-    // creates a ray for a sliding piece, in one of 8 directions: up, left,
-    // right, down, up left, up right, down left, down right. (Some
-    // programs call it N, E, S, W, NW, NE, SE, SW.) The function starts at
-    // the given square, in a given direction, and then it keeps iterating
-    // in that direction until it either hits a piece, or the edge of the
-    // board. Therefore, in each call, only one of the eight blocks of this
-    // function will be executed.
-    pub fn bb_ray(bb_in: &Bitboard, square: Square, direction: Direction) -> Bitboard {
+    // get_edges generates a bitboard of all the edges of the board excluding
+    // the given square.
+    //
+    // @param: exclude - square to exclude from the edges
+    // @return: bitboard of all the edges of the board
+    // TODO: think about moving this function elsewhere
+    fn get_edges(exclude: Square) -> Bitboard {
+        let exclude_file = BITBOARD_FILES[exclude.file()];
+        let exclude_rank = BITBOARD_RANKS[exclude.rank()];
+
+        (BITBOARD_FILES[Files::A] & !exclude_file)
+            | (BITBOARD_FILES[Files::H] & !exclude_file)
+            | (BITBOARD_RANKS[Ranks::R1] & !exclude_rank)
+            | (BITBOARD_RANKS[Ranks::R8] & !exclude_rank)
+    }
+
+    // attack_ray returns the attack ray from the current square in the given
+    // direction based on the given bitboard.
+    //
+    // @param: bitboard - bitboard to use as the base for the attack ray
+    // @param: square - square to start the attack ray from
+    // @param: direction - direction to attack in
+    // @return: attack ray bitboard
+    pub fn attack_ray(bitboard: &Bitboard, square: Square, direction: Direction) -> Bitboard {
+        // get the file and rank and the square to analyze
         let mut file = square.file();
         let mut rank = square.rank();
-        let mut bb_square = BITBOARD_SQUARES[square.unwrap()];
-        let mut bb_ray = Bitboard::empty();
-        let mut done = false;
-        while !done {
-            done = true;
+        let mut square = BITBOARD_SQUARES[square.unwrap()];
+
+        // build the ray bitboard in the given direction
+        let mut ray = Bitboard::empty();
+        loop {
             match direction {
                 Direction::Up => {
-                    if rank != Ranks::R8 {
-                        bb_square <<= 8u8;
-                        bb_ray |= bb_square;
-                        rank += 1;
-                        done = !(bb_square & bb_in).is_empty();
+                    if rank == Ranks::R8 {
+                        break;
                     }
+
+                    square <<= 8u8;
+                    ray |= square;
+                    rank += 1;
                 }
                 Direction::Right => {
-                    if file != Files::H {
-                        bb_square <<= 1u8;
-                        bb_ray |= bb_square;
-                        file += 1;
-                        done = !(bb_square & bb_in).is_empty();
+                    if file == Files::H {
+                        break;
                     }
+
+                    square <<= 1u8;
+                    ray |= square;
+                    file += 1;
                 }
                 Direction::Down => {
-                    if rank != Ranks::R1 {
-                        bb_square >>= 8u8;
-                        bb_ray |= bb_square;
-                        rank -= 1;
-                        done = !(bb_square & bb_in).is_empty();
+                    if rank == Ranks::R1 {
+                        break;
                     }
+
+                    square >>= 8u8;
+                    ray |= square;
+                    rank -= 1;
                 }
                 Direction::Left => {
-                    if file != Files::A {
-                        bb_square >>= 1u8;
-                        bb_ray |= bb_square;
-                        file -= 1;
-                        done = !(bb_square & bb_in).is_empty();
+                    if file == Files::A {
+                        break;
                     }
+
+                    square >>= 1u8;
+                    ray |= square;
+                    file -= 1;
                 }
                 Direction::UpLeft => {
-                    if (rank != Ranks::R8) && (file != Files::A) {
-                        bb_square <<= 7u8;
-                        bb_ray |= bb_square;
-                        rank += 1;
-                        file -= 1;
-                        done = !(bb_square & bb_in).is_empty();
+                    if rank == Ranks::R8 || file == Files::A {
+                        break;
                     }
+
+                    square <<= 7u8;
+                    ray |= square;
+                    rank += 1;
+                    file -= 1;
                 }
                 Direction::UpRight => {
-                    if (rank != Ranks::R8) && (file != Files::H) {
-                        bb_square <<= 9u8;
-                        bb_ray |= bb_square;
-                        rank += 1;
-                        file += 1;
-                        done = !(bb_square & bb_in).is_empty();
+                    if rank == Ranks::R8 || file == Files::H {
+                        break;
                     }
+
+                    square <<= 9u8;
+                    ray |= square;
+                    rank += 1;
+                    file += 1;
                 }
                 Direction::DownRight => {
-                    if (rank != Ranks::R1) && (file != Files::H) {
-                        bb_square >>= 7u8;
-                        bb_ray |= bb_square;
-                        rank -= 1;
-                        file += 1;
-                        done = !(bb_square & bb_in).is_empty();
+                    if rank == Ranks::R1 || file == Files::H {
+                        break;
                     }
+
+                    square >>= 7u8;
+                    ray |= square;
+                    rank -= 1;
+                    file += 1;
                 }
                 Direction::DownLeft => {
-                    if (rank != Ranks::R1) && (file != Files::A) {
-                        bb_square >>= 9u8;
-                        bb_ray |= bb_square;
-                        rank -= 1;
-                        file -= 1;
-                        done = !(bb_square & bb_in).is_empty();
+                    if rank == Ranks::R1 || file == Files::A {
+                        break;
                     }
+
+                    square >>= 9u8;
+                    ray |= square;
+                    rank -= 1;
+                    file -= 1;
                 }
             };
+
+            // if the square is blocked, we have built the full ray in this
+            // direction, so we can stop
+            if !(square & bitboard).is_empty() {
+                break;
+            }
         }
-        bb_ray
+
+        ray
     }
 }
