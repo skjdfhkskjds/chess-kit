b4ad9353269ccf12693859a62436650877540f8d
skjdfhkskjds <colin.chuu@gmail.com>
2025-11-29 15:47:29 -0500

    make the default piece value None instead of Pawn

--- src/primitives/pieces/piece.rs
+++ src/primitives/pieces/piece.rs
@@ -3,15 +3,16 @@ use chess_kit_derive::IndexableEnum;
 #[repr(u8)]
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Debug, IndexableEnum)]
 pub enum Piece {
+    None,
     Pawn,
     Knight,
     Bishop,
     Rook,
     Queen,
     King,
-    None,
 }
 
 impl Piece {
     pub const TOTAL: usize = 7;
 }
+ 
