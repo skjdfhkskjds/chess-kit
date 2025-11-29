353b2766e65bdb1e7d8e719b650c17fc4e1aeefe
skjdfhkskjds <colin.chuu@gmail.com>
2025-11-29 01:58:31 -0500

    move sides into its own folder for consistency with the other primitives

--- src/primitives/mod.rs
+++ src/primitives/mod.rs
@@ -2,12 +2,12 @@ pub mod bitboard;
 pub mod castling;
 pub mod moves;
 pub mod pieces;
-pub mod side;
+pub mod sides;
 pub mod squares;
 
 pub use bitboard::{BITBOARD_FILES, BITBOARD_RANKS, BITBOARD_SQUARES, Bitboard, BitboardVec};
 pub use castling::Castling;
 pub use moves::{Move, MoveList, MoveType, ShortMove};
 pub use pieces::{Piece, Pieces};
-pub use side::Side;
+pub use sides::Side;
 pub use squares::{File, Files, Rank, Ranks, Square, Squares};
---
+++ src/primitives/sides/mod.rs
@@ -0,0 +1,3 @@
+pub mod side;
+
+pub use side::*;
