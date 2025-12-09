fca6dcf14a8ec9c38505d267e0b32421e63eae0c
skjdfhkskjds <colin.chuu@gmail.com>
2025-12-09 17:25:27 -0500

    reenable tests for benchmarking

--- src/primitives/bitboard/bitboard.rs
+++ src/primitives/bitboard/bitboard.rs
@@ -114,7 +114,7 @@ impl Bitboard {
     // @return: true if the bitboard has more than one bit set, false otherwise
     #[inline(always)]
     pub const fn more_than_one(&self) -> bool {
-        self.0.count_ones() > 1
+        self.0 & (self.0 - 1) != 0
     }
 
     // intersects checks whether there is an intersection between this and the
